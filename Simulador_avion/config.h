7c9afbc0b08d121285df34adcf40eaeec32dac40
AngelGutierrez <angeldavid.guti.con>
2026-07-24 19:11:38 -0400

    ya funciona

--- Simulador_avion/config.h
+++ Simulador_avion/config.h
@@ -8,7 +8,7 @@
 
 // RUTA DEL MODELO 3D A CARGAR
 // Cambia esta ruta a tu modelo .obj o .fbx
-#define MODEL_PATH "C:/Users/angel/Desktop/Universidad/8vo/Computacion Grafica/Proyectos/Proyecto 2/Simulador_avion/Simulador_avion/source/Millennium Falcon star wars.obj"
+#define MODEL_PATH "C:/Users/angel/Desktop/Universidad/8vo/Computacion Grafica/Proyectos/Proyecto 2/Simulador_avion/Simulador_avion/source/MYSTERE IV N 117 Provence Nancy.fbx"
 
 // Escala del modelo (ajusta si es muy grande o pequeño)
 #define MODEL_SCALE 0.1f
